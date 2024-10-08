2d1ccd1ee175e8097ebecc41c52150727d4323ec
noeldesalegn <desalen2121@gmail.com>
2024-10-08 09:34:56 -0700

    livewire 1'st push

---
+++ resources/views/livewire/clicker.blade.php
@@ -0,0 +1,6 @@
+<div>
+    {{-- Be like water. --}}
+    <button wire:click="handleClick" style=" background-color: blue; color: white; padding: 10px 20px; border: none; border-radius: 5px; margin: 20px;">
+        Click on me
+    </button>
+    </div>
