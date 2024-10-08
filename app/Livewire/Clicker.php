6423fc7398558bce0fa5ef9d71703050d7ae2d26
noeldesalegn <desalen2121@gmail.com>
2024-10-08 12:16:30 -0700

    can add or create new user

--- app/Livewire/Clicker.php
+++ app/Livewire/Clicker.php
@@ -2,15 +2,27 @@
 
 namespace App\Livewire;
 
+use App\Models\User;
 use Livewire\Component;
 
 class Clicker extends Component
 {
-    public function handleClick(){
-        dump("clicked");
+    public $username = "test user";
+    public function CreateNewUser(){
+        User::create([
+            'name' => "test user 3",
+            'email' => "test@gmail3.com",
+            'password' => "1234567899"
+        ]);
     }
+
     public function render()
     {
-        return view('livewire.clicker');
+        $title="test";
+        $users=User::all();
+        return view('livewire.clicker',[
+            'title' => $title,
+            'users' => $users
+        ]);
     }
 }
--- resources/views/livewire/clicker.blade.php
+++ resources/views/livewire/clicker.blade.php
@@ -1,6 +1,10 @@
 <div>
     {{-- Be like water. --}}
-    <button wire:click="handleClick" style=" background-color: blue; color: white; padding: 10px 20px; border: none; border-radius: 5px; margin: 20px;">
-        Click on me
+    <h2>{{ $username }}</h2>
+    <h1>{{ $title}}</h1>
+    {{ count($users) }}
+    <br>
+    <button wire:click=" CreateNewUser" style=" background-color: blue; color: white; padding: 10px 20px; border: none; border-radius: 5px; margin: 20px;">
+        Create new user
     </button>
     </div>
